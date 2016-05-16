c21d3003c4b73bb46f099d482dec0994130a6fd5
adsff123 <adsff123@gmail.com>
2016-05-16 07:36:09 +0000

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def main
+  end
+  
+  def uuuu
+  end
+  
+  def login
+  end
+  
+  def mypage
+  end
+  
+  def signup
+  end
+  
+  def teambuild
+  end
+  
+  def teamlist
+  end
+end
